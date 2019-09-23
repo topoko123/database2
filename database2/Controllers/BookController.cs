7481f9997b1c38a5383c0d9d1a6b5288f846239a
Thanwa <aunkerzerza@hotmail.com>
2019-09-23 20:15:05 +0700

    first commit

---
+++ database2/Controllers/BookController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using database2.Data;
+using database2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace database2.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public int Id { get; private set; }
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await _context.Books.ToListAsync();
+            return View(model);
+        }
+        [Authorize]
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddBook(Book model)
+        {
+           if (ModelState.IsValid)
+            {
+                var oldBook = await _context.Books.AnyAsync(b => b.Name == model.Name);
+                if (oldBook)
+                {
+                    return View();
+                }
+                else
+                {
+                    _context.Books.Add(model);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+        [Authorize]
+        public async Task<IActionResult> BookEdit(int Id)
+        { 
+            
+                var book = await _context.Books.FirstOrDefaultAsync(prawit => prawit.Id == Id);
+                if (book == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(book);
+                }
+            
+
+        }
+        [HttpPost]
+        [Authorize] 
+        public async Task<IActionResult> EditBook(Book model)
+        {
+            if (ModelState.IsValid)
+            {
+                var book = await _context.Books.FirstOrDefaultAsync(prawit => prawit.Id == model.Id);
+                if (book != null)
+                {
+                    book.Name = model.Name;
+                    book.Price = model.Price;
+                    await _context.SaveChangesAsync();
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> BookDelete(int Id)
+        {
+            var book = await _context.Books.FirstOrDefaultAsync(prawit => prawit.Id == Id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+            
+        }
+       
+    }
+}
---
+++ database2/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using database2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace database2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
